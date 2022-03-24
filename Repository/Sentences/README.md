8ce826f542b8de644661c38e5658cf908e87487e
lesserfish <lesserfish@protonmail.com>
2022-03-24 00:49:23 -0300

    db now has english translations

--- Repository/Sentences/README.md
+++ Repository/Sentences/README.md
@@ -1,1 +1,3 @@
 Sentences obtained from the incredible Tatoeba project, https://tatoeba.org/en/, released under the CC-BY 2.0 FR license.
+
+Download link: ftp://ftp.edrdg.org/pub/Nihongo/examples.utf.gz
--- Tools/SentenceTokenizer/CreateDB.py
+++ Tools/SentenceTokenizer/CreateDB.py
@@ -2,20 +2,20 @@ from janome.tokenizer import Tokenizer
 import sqlite3
 
 DB = "../../Database/Sentences.db"
-DICT = "../../Repository/Sentences/jpn_sentences.tsv"
+DICT = "../../Repository/Sentences/JP-ENG-PAIRS.tsv"
 
 tokenizer = Tokenizer()
 conn = sqlite3.connect(DB)
 print("Creating table...")
 try:
     conn.execute("CREATE TABLE wordmap (word TEXT, id INTEGER);")
-    conn.execute("CREATE TABLE sentmap (id INTEGER, sentence TEXT);")
+    conn.execute("CREATE TABLE sentmap (id INTEGER, sentence TEXT, translation TEXT);")
     print("Success!")
 except sqlite3.OperationalError:
     conn.execute("DROP TABLE wordmap")
     conn.execute("DROP TABLE sentmap")
     conn.execute("CREATE TABLE wordmap (word TEXT, id INTEGER);")
-    conn.execute("CREATE TABLE sentmap (id INTEGER, sentence TEXT);")
+    conn.execute("CREATE TABLE sentmap (id INTEGER, sentence TEXT, translation TEXT);")
     print("Table already exists. Recreated!")
 
 sfile = open(DICT, "r")
@@ -25,11 +25,12 @@ id = 0
 for sentence in sentences:
     sep = sentence.split("\t")
     id = id + 1
-    result = str(sep[2])
+    sentence = str(sep[1])
+    translation = str(sep[3])
     
-    conn.execute("INSERT INTO sentmap VALUES (?, ?)", (id, result))
+    conn.execute("INSERT INTO sentmap VALUES (?, ?, ?)", (id, sentence, translation))
 
-    for token in tokenizer.tokenize(result, wakati=True):
+    for token in tokenizer.tokenize(sentence, wakati=True):
         conn.execute("INSERT INTO wordmap VALUES (?, ?)", (str(token), id))
     
 conn.commit()

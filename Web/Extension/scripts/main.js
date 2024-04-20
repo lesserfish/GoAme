f34a7e8f18fc3ea6b94d8245dbeac336f8b1baf8
lesserfish <lesserfish@pm.me>
2024-04-20 00:48:20 -0300

    The WebExtension has been completely rewritten.
    It is working fine. And it is definitely much much more readable. Now we only need to update the popup!
    Also, expressions work. Multiple searches work. And weird words that used to not work, now work!!!!!

--- Web/Extension/scripts/main.js
+++ Web/Extension/scripts/main.js
@@ -1,2 +1,139 @@
-$(function(){
+class AmeExtension {
+    async Main() {
+        var storage = new Storage();
+        var memory = await storage.LoadMemory();
+        this.storage = storage;
+        this.memory = memory;
+
+        this.#Go();
+        this.#CreateObserver();
+        
+    }
+
+    async #Go() {
+        $(".concept_light.clearfix").each((index, element) => this.#HandleEntry(index, element));
+    }
+
+    async #HandleEntry(index, element)
+    {
+        if($(element).parent().attr("class") == "names") {
+            return;
+        }
+        var eState = $(element).find(".concept_light-status");
+        var reading = this.#GetReadings(element);
+
+        var inMemory = this.memory.some(function(entry) {
+            return (entry.w == reading.w && entry.k == reading.k);
+        });
+
+        this.#InjectFunctionality(inMemory, eState);
+    }
+
+    #GetReadings(jObject)
+    {
+        var furigana = [];
+
+        $(jObject).find(".concept_light-readings").find(".furigana").find(".kanji").each(function(index, element){
+            furigana.push($(this).text());
+        });
+
+        var aWord = [];
+        var aKana = [];
+
+        $(jObject).find(".concept_light-readings").find(".text").contents().each(function(index, element){
+            var content = $(this).text().replace(/\s/g, "");
+            if(this.nodeName.toLowerCase() == "span"){
+                aWord.push(content);
+                aKana.push(content);
+            }
+            else {
+                aWord.push(content);
+                aKana.push(furigana.shift() || "");
+            }
+        });
+
+        var word = aWord.join("");
+        var kana = aKana.join("");
+
+        return({w: word, k: kana});
+    }
+
+    async #InjectFunctionality(inMemory, eState){
+        if(inMemory) {
+            var newLink = $("<a href='#' class=''>Remove from memory</a>");
+            newLink.click((e) => this.#DeregisterElement(e, newLink));
+            eState.append(newLink);
+        }
+        else {
+            var newLink = $("<a href='#' class=''>Add to memory</a>");
+            newLink.click((e) => this.#RegisterElement(e, newLink));
+            eState.append(newLink);
+        }
+
+    }
+    async #RegisterElement(e, linkObject){
+        var eParent = $(linkObject).parent();
+        this.#AddToMemory(eParent);
+        $(linkObject).remove();
+        this.#InjectFunctionality(true, eParent);
+        e.preventDefault();
+    }
+    async #DeregisterElement(e, linkObject){
+        var eParent = $(linkObject).parent();
+        this.#RemoveFromMemory(eParent);
+        $(linkObject).remove();
+        this.#InjectFunctionality(false, eParent);
+        e.preventDefault();    
+    }
+
+    async #AddToMemory(eState) {
+        var entry = $(eState).closest(".concept_light.clearfix");
+        var reading = this.#GetReadings(entry);
+        this.memory.push(reading);
+        this.storage.SaveMemory(this.memory);
+        console.log(this.memory);
+    }
+
+    async #RemoveFromMemory(eState) {
+        var element = $(eState).closest(".concept_light.clearfix");
+        var reading = this.#GetReadings(element);
+        this.memory = this.memory.filter(function(entry){
+            return !(entry.w == reading.w && entry.k == reading.k);
+        });
+        this.storage.SaveMemory(this.memory);
+        console.log(this.memory);
+    }
+
+    async #CreateObserver(){
+
+        // Select the node that you want to observe for mutations
+        const targetNode = document.body; // You can choose any parent node where "primary" div might be added
+        const config = { childList: true, subtree: true };
+        const observer = new MutationObserver((m, o) => this.#HandleMutation(m, o));
+
+        // Start observing the target node for configured mutations
+        observer.observe(targetNode, config);
+    }
+
+    async #HandleMutation(mutationsList, observer) {
+        // Loop through the list of mutations
+        for(const mutation of mutationsList) {
+            // Check if nodes were added
+            if (mutation.type === 'childList') {
+                // Loop through added nodes
+                mutation.addedNodes.forEach(node => {
+                    // Check if the added node is a div with the id "main_results"
+                    if (node.nodeType === 1 && node.id === 'main_results') {
+                        this.#Go();
+                    }
+                });
+            }
+        }
+    }
+}
+
+$(async function(){
+    ame = new AmeExtension();
+    ame.Main();
 }); 
+
--- Web/Extension/scripts/memory.js
+++ Web/Extension/scripts/memory.js
@@ -1,93 +1,92 @@
 var MAX_SEG_SIZE = 8000
-var Dictionary = []
 
-function ByteSize(str)
-{
-    return (new TextEncoder().encode(str)).length;
-}
-
-function ChunkName(chunk_id) {
-    return "chunk_" + chunk_id.toString();
-}
-
-function EntryToString(entry)
-{
-    var output = entry['k'] + ":" + entry['h'];
-    return output;
-}
-
-function LoadChunk(chunk) {
-    var entries = chunk.split('\n');
-    for(var i = 0; i < entries.length; i++)
+class Storage {
+    #ByteSize(str)
     {
-        entry = entries[i];
-        if(entry.length == 0){
-            continue
-        }
-        var k = "";
-        var h = "";
-        var components = entry.split(':');
-        k = components[0];
-        if(components.length > 1){
-            h = components[1];
-        }
-        var word = {
-          k: k,
-          h: h,
-        }
-        Dictionary.push(word)
+        return (new TextEncoder().encode(str)).length;
     }
-}
-async function SaveChunk(chunk, chunk_id){
-    var chunk_name = ChunkName(chunk_id)
-    var map = {};
-    map[chunk_name] = chunk; 
-    await browser.storage.sync.set(map);
-}
-async function LoadDictionary() {
-    var promise = browser.storage.sync.get();
-    promise.then( 
-        async function(storage) {
-            if("chunk_count" in storage){
-                for(var chunk_id = 1; chunk_id <= storage['chunk_count']; chunk_id++)
-                {
-                    var chunk_name = ChunkName(chunk_id);
-                    if(!chunk_name in storage)
-                    {
+
+    #ChunkName(chunk_id) {
+        return "chunk_" + chunk_id.toString();
+    }
+
+    #EntryToString(entry)
+    {
+        var output = entry['w'] + ":" + entry['k'];
+        return output;
+    }
+
+    #LoadChunk(chunk, memory) {
+        var entries = chunk.split('\n');
+        for(var i = 0; i < entries.length; i++)
+        {
+            var entry = entries[i];
+            if(entry.length == 0){
+                continue
+            }
+            var w = "";
+            var k = "";
+            var components = entry.split(':');
+            w = components[0];
+            if(components.length > 1){
+                k = components[1];
+            }
+            var word = {
+              w: w,
+              k: k,
+            }
+            memory.push(word)
+        }
+    }
+    async #SaveChunk(chunk, chunk_id){
+        var chunk_name = this.#ChunkName(chunk_id)
+        var map = {};
+        map[chunk_name] = chunk; 
+        await browser.storage.sync.set(map);
+    }
+
+    async LoadMemory() {
+        var memory = [];
+        try {
+            var storage = await browser.storage.sync.get();
+            if ("chunk_count" in storage) {
+                for (var chunk_id = 1; chunk_id <= storage['chunk_count']; chunk_id++) {
+                    var chunk_name = this.#ChunkName(chunk_id);
+                    if (!(chunk_name in storage)) {
                         console.error("CRITICAL ERROR: Attempted to get non-existent chunk");
                     }
-                    LoadChunk(storage[chunk_name])
+                    await this.#LoadChunk(storage[chunk_name], memory);
                 }
             } else {
-                SaveDictionary();
+                this.SaveMemory(memory);
             }
-        },
-        async function(e) {
+            return memory; // Return the populated memory array
+        } catch (e) {
             console.error("Error: " + e);
-        });
-    return await promise
-}
-
-
-async function SaveDictionary() {
-    var chunk_count = 1;
-    var current_chunk = ""
-    for(var i = 0; i < Dictionary.length; i++)
-    {
-        var entry = EntryToString(Dictionary[i])
-        if(ByteSize(current_chunk) + ByteSize("\n") + ByteSize(entry) > MAX_SEG_SIZE)
-        {
-            SaveChunk(current_chunk, chunk_count);
-            current_chunk = entry;
-            chunk_count += 1;
-        } else {
-            if(current_chunk.length == 0){
-                current_chunk = entry;
-            } else {
-                current_chunk = current_chunk + "\n" + entry;
-            }
+            return []; // Return an empty array in case of error
         }
     }
-    SaveChunk(current_chunk, chunk_count);
-    await browser.storage.sync.set({chunk_count: chunk_count})
+
+    async SaveMemory(memory) {
+        var chunk_count = 1;
+        var current_chunk = ""
+        for(var i = 0; i < memory.length; i++)
+        {
+            var entry = this.#EntryToString(memory[i])
+            if(this.#ByteSize(current_chunk) + this.#ByteSize("\n") + this.#ByteSize(entry) > MAX_SEG_SIZE)
+            {
+                this.#SaveChunk(current_chunk, chunk_count);
+                current_chunk = entry;
+                chunk_count += 1;
+            } else {
+                if(current_chunk.length == 0){
+                    current_chunk = entry;
+                } else {
+                    current_chunk = current_chunk + "\n" + entry;
+                }
+            }
+        }
+        this.#SaveChunk(current_chunk, chunk_count);
+        await browser.storage.sync.set({chunk_count: chunk_count})
+    }
 }
